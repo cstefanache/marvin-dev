1f9f0767d9ef73e8ff3cde848d7628c7c51a3ceb
Raluca Brie <ralucabrie2014.br@gmail.com>
2022-12-14 18:17:54 +0200

    unit tests - tag only

--- packages/discovery/src/tests/mixed-exclusion-rules.test.ts
+++ packages/discovery/src/tests/mixed-exclusion-rules.test.ts
@@ -1,6 +1,6 @@
-import Discovery from '../../discovery';
-import {Config} from '../../models/config';
-import {PageDiscoveryResult} from '../../models/models';
+import Discovery from '../discovery';
+import {Config} from '../models/config';
+import {PageDiscoveryResult} from '../models/models';
 
 describe('Test discovery', () => {
     it('exclusion - id and tag property', async () => {
