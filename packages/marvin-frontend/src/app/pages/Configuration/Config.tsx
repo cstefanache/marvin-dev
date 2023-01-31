bd45b64516fe85409ac045e22c6690f762031aaf
Cornel Stefanache <cornel.stefanache@gmail.com>
2023-01-31 09:54:01 +0200

    added save confirmation

--- packages/marvin-frontend/src/app/pages/Configuration/Config.tsx
+++ packages/marvin-frontend/src/app/pages/Configuration/Config.tsx
@@ -1,21 +1,22 @@
-import { ReactNode, useEffect, useState, useMemo } from 'react';
+import { ReactNode, useEffect, useState, useMemo, useRef } from 'react';
 
 import { SchemaForm } from '@ascentcore/react-schema-form';
 import ConfigSchema from '../../schemas/config.schema.json';
-import { CustomRegistry, CustomWrapper } from '../../components/Registry/Wrapper/Wrapper';
+import {
+  CustomRegistry,
+  CustomWrapper,
+} from '../../components/Registry/Wrapper/Wrapper';
 import { JSONObject } from '../../types/Types';
 import { TabContext } from '../../contexts/TabContext';
+import { Position, Toaster } from '@blueprintjs/core';
 
 import './ConfigStyles.scss';
 
 export default function Config() {
   const [config, setConfig] = useState<JSONObject | undefined>();
   const [tab, setTab] = useState(0);
-  const contextValue = useMemo(
-    () => ({ tab, setTab }),
-    [tab]
-  );
-
+  const contextValue = useMemo(() => ({ tab, setTab }), [tab]);
+  const toastRef = useRef(null);
   useEffect(() => {
     const asyncFn = async () => {
       const config = await window.electron.getConfig();
@@ -28,6 +29,13 @@ export default function Config() {
     try {
       window.electron.setConfig(data);
       setConfig(data);
+      if (toastRef.current) {
+        (toastRef.current as Toaster).show({
+          intent: 'primary',
+          message: 'Config saved',
+          timeout: 3000,
+        });
+      }
     } catch (e) {
       console.log(e);
     }
@@ -35,6 +43,7 @@ export default function Config() {
 
   return config ? (
     <TabContext.Provider value={contextValue}>
+      <Toaster position={Position.TOP} ref={toastRef} />
       <SchemaForm
         className="config-container"
         schema={ConfigSchema}
