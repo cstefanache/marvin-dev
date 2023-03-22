4331c5159ea38a0a022b66de0d2ea96b5386c692
Raluca Brie <ralucabrie2014.br@gmail.com>
2023-03-22 09:16:13 +0200

    minor fix

--- packages/generators/cypress/src/main.ts
+++ packages/generators/cypress/src/main.ts
@@ -2,9 +2,9 @@ import Structure from './structure';
 import CypressCodeGenerator from './code';
 
 async function run() {
-  const structure = new Structure('./tmp/LocalTriblio');
+  const structure = new Structure('./output');
 
-  const code = new CypressCodeGenerator(structure.flow, structure.config, 'packages/sample-frontend-e2e/src');
+  const code = new CypressCodeGenerator(structure.flow, structure.config);
   await code.generate();
 }
 
