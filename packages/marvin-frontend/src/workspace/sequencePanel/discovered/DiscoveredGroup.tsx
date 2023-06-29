48c1a85443b2fc5fa61fb65dfcf6dd188de2a9de
Cornel Stefanache <cornel.stefanache@gmail.com>
2023-06-29 14:16:26 +0300

    Updated UI for discovered groups

--- packages/marvin-frontend/src/workspace/sequencePanel/discovered/DiscoveredGroup.tsx
+++ packages/marvin-frontend/src/workspace/sequencePanel/discovered/DiscoveredGroup.tsx
@@ -1,21 +1,33 @@
 import { Callout, Tag } from '@blueprintjs/core';
 import { getIcon } from '../../../utils';
 import { Collapse, Icon } from '@blueprintjs/core';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function DiscoveredGroup({
   discoveredElements,
   filter,
   category,
-  label
+  label,
 }: {
   discoveredElements: any;
   filter: any;
   category: string;
   label: string;
 }) {
-
   const [isOpen, setIsOpen] = useState(false);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setList(
+      discoveredElements.filter(
+        (item: any) =>
+          item.from === category &&
+          (filter.trim().length === 0 ||
+            (item.text || '').toLowerCase().includes(filter.toLowerCase()) ||
+            (item.details || '').toLowerCase().includes(filter.toLowerCase()))
+      )
+    );
+  }, [discoveredElements, filter]);
 
   return (
     <>
@@ -25,41 +37,27 @@ export default function DiscoveredGroup({
           setIsOpen(!isOpen);
         }}
       >
-        {label}
-        <Icon
-          className={`chevron ${isOpen ? 'down' : 'up'}`}
-          icon="chevron-left"
-        />
+        <Icon icon={isOpen ? 'chevron-down' : 'chevron-right'} />
+        {label} ({list.length})
       </div>
       <Collapse isOpen={isOpen}>
-        {discoveredElements
-          .filter(
-            (item: any) =>
-              filter.trim().length === 0 ||
-              (item.text || '').toLowerCase().includes(filter.toLowerCase()) ||
-              (item.details || '').toLowerCase().includes(filter.toLowerCase())
-          )
-          .map(
-            (item: any) =>
-              item.from === category && (
-                <Callout
-                  key={item.locator}
-                  icon={getIcon(item)}
-                  className="mt-2"
-                >
-                  {item.text} | {item.details}
-                  <div>
-                    <Tag minimal={true}>{item.locator}</Tag>
-                  </div>
-                  {item.base64Image && (
-                    <img
-                      src={`data:image/png;base64,${item.base64Image}`}
-                      alt="icon"
-                    />
-                  )}
-                </Callout>
-              )
-          )}
+        {list.map(
+          (item: any) =>
+            item.from === category && (
+              <Callout key={item.locator} icon={getIcon(item)} className="mt-2">
+                {item.text} | {item.details}
+                <div>
+                  <Tag minimal={true}>{item.locator}</Tag>
+                </div>
+                {item.base64Image && (
+                  <img
+                    src={`data:image/png;base64,${item.base64Image}`}
+                    alt="icon"
+                  />
+                )}
+              </Callout>
+            )
+        )}
       </Collapse>
     </>
   );
