543d6b8df6def7f9a4ff4ce13105d3bc8d9c13ff
Baraian Camelia <camelia.baraian@rover.com>
2023-07-11 16:24:54 +0300

    add sequenceContainer

--- packages/marvin-frontend/src/workspace/sequencePanel/addMethod/CreateMethod.tsx
+++ packages/marvin-frontend/src/workspace/sequencePanel/addMethod/CreateMethod.tsx
@@ -190,7 +190,11 @@ const CreateMethod = (props: any) => {
           sequence: {
             ...CreateSchema.properties.sequence,
             enum: items.reduce( (memo, item) => { memo.push( item.locator); return memo}, [])
-          }}
+          }},
+          sequenceContainer:{
+            ...CreateSchema.properties.sequenceContainer.properties.locator,
+            enum:options
+          }
         })
         setItems(items);
       }
@@ -207,6 +211,7 @@ const CreateMethod = (props: any) => {
     }
   }, []);
   console.log(schema)
+  console.log('bgfh',items)
   async function doSave() {
     const saveObject: any = {
       uid,
